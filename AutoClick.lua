0bebf12da5dd428110f09332f3a7722590094ecd
SirShinHub <93308719+SirShinHub@users.noreply.github.com>
2023-08-23 06:49:51 +0700

    Create AutoClick.lua

---
+++ AutoClick.lua
@@ -0,0 +1,8 @@
+getgenv().Settings = {
+    ["Auto Click Keybind"] = "KeypadZero",
+    ["Lock Mouse Position Keybind"] = "KeypadOne",
+    ["Right Click"] = false,
+    ["GUI"] = true,
+    ["Delay"] = 0
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BimbusCoder/Script/main/Auto%20Clicker.lua"))()
