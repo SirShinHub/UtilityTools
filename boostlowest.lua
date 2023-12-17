28841c95e229544cf204ce8c81e7c0595e758d61
SirShinHub <93308719+SirShinHub@users.noreply.github.com>
2023-12-17 21:33:48 +0700

    Create boostlowest.lua

---
+++ boostlowest.lua
@@ -0,0 +1,46 @@
+_G.WaitPerAmount = 500
+_G.Settings = {
+    Players = {
+            ["Ignore Me"] = true,
+            ["Ignore Others"] = true,
+        },
+        Meshes = {
+            NoMesh = false,
+            NoTexture = true,
+            Destroy = false,
+		LowDetail = true
+        },
+        Images = {
+            Invisible = true,
+            Destroy = false
+        },
+        Particles = {
+            Invisible = false,
+            Destroy = false
+        },
+        TextLabels = {
+            LowerQuality = false,
+            Invisible = false,
+            Destroy = false
+        },
+        MeshParts = {
+            LowerQuality = true,
+            Invisible = false,
+            NoTexture = true,
+            NoMesh = false,
+            Destroy = false
+        },
+	    ["No Explosions"] = true,	
+	    ["No Particles"] = true, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparksle
+            ["FPS Cap"] = 144, -- Set this true to uncap FPS
+            ["No Camera Effects"] = true,
+            ["No Clothes"] = true,
+            ["Low Water Graphics"] = true,
+            ["No Shadows"] = true,
+            ["Low Rendering"] = true,
+            ["Low Quality Parts"] = true,
+            ["Low Quality Models"] = true,
+            ["Reset Materials"] = false,
+            ["Lower Quality MeshParts"] = true
+        }
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SirShinHub/UtilityTools/main/fpsboostlowestLIB.lua"))()
